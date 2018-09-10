765927e3f6cbf7a67c2e4af4bd5f059ec2f7ed91
Dan Hahn <daniel.hahn@michaelkors.com>
2018-09-10 16:09:29 -0400

    Add render props for mobile view

--- src/components/pdp-gallery/index.jsx
+++ src/components/pdp-gallery/index.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
-import styled from 'styled-components';
+import styled, { css } from "styled-components";
 import PdpMobile from './mobile';
 import PdpDesktop from './desktop';
 
 const PdpGalleryWrapper = styled.div`
   margin-top: 500px;
   margin-bottom: 5000px;
+`;
+
+const Img = styled.img`
+  display: block;
+  max-width: 100%;
+`;
+
+const Thumbnail = styled(Img)`
+  opacity: 0.7;
+  ${props =>
+    props.active &&
+    css`
+      opacity: 1;
+    `};
 `;
 
 export default class PdpGallery extends Component {
@@ -33,7 +47,18 @@ export default class PdpGallery extends Component {
         {!this.isMobile() ? (
           <PdpDesktop photos={this.props.photos} />
         ) : (
-          <PdpMobile photos={this.props.photos} />
+          <PdpMobile photos={this.props.photos}
+            render={() => (this.props.photos.map(photo => (
+              <div key={photo.id}>
+                <Img src={photo.url} alt="" />
+              </div>
+            )))}
+            renderThumbnails={(updateCurrentSlide, activeIndex) => (this.props.photos.map((photo, index) => (
+              <div key={photo.id} onClick={() => updateCurrentSlide(index)}>
+                <Thumbnail src={photo.url} active={index === activeIndex} alt="" />
+              </div>
+            )))}
+          />
         )}
       </PdpGalleryWrapper>;
   }
--- src/components/pdp-gallery/mobile.jsx
+++ src/components/pdp-gallery/mobile.jsx
@@ -1,12 +1,75 @@
 import React, { Component } from "react";
 import Swiper from "react-id-swiper/lib/custom";
+import styled, { css } from 'styled-components';
+
+const PdpGalleryWrapper = styled.div`
+  border: 1px solid black;
+`;
+
+const Img = styled.img`
+  display: block;
+  max-width: 100%;
+`;
+
+const Thumbnail = styled(Img)`
+  opacity: 0.7;
+  ${props =>
+    props.active &&
+    css`
+      opacity: 1;
+    `};
+`;
+
+const ThumbnailsWrapper = styled.div`
+  overflow: hidden;
+`;
+
+const Thumbnails = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: grid;
+  grid-gap: 10px;
+  margin-top: 10px;
+  grid-template-columns: 44px;
+  grid-auto-flow: column;
+  grid-auto-columns: 44px;
+`;
 
 class PdpMobile extends Component {
   static defaultProps = {
     photos: []
+  };
+
+  constructor(props) {
+    super(props);
+    this.swiper = null;
+    this.updateCurrentIndex = this.updateCurrentIndex.bind(this);
+    this.updateCurrentSlide = this.updateCurrentSlide.bind(this);
+    this.state = {
+      swiper: "1234",
+      activeIndex: 0
+    };
   }
+
+  componentDidMount() {
+    this.setState({ swiper: this.swiper });
+    this.swiper.el.addEventListener("touchend", this.updateCurrentIndex);
+  }
+
+  updateCurrentIndex() {
+    const {activeIndex} = this.swiper;
+    this.setState({ activeIndex });
+  }
+
+  updateCurrentSlide(nextSlide) {
+    this.swiper.slideTo(nextSlide, 200);
+    this.setState({ activeIndex: nextSlide });
+  }
+
   render() {
-    const { photos } = this.props;
+    const { photos, render, renderThumbnails } = this.props;
+    const { activeIndex } = this.state;
     const params = {
       breakpoints: {
         510: {
@@ -16,15 +79,20 @@ class PdpMobile extends Component {
         }
       }
     };
-    return (
-      <Swiper {...params} id="plp">
-        {photos.map(photo => (
-          <div key={photo.id}>
-            <img src={photo.url} alt="" style={{maxWidth: '100%'}} />
-          </div>
-        ))}
-      </Swiper>
-    );
+    return <PdpGalleryWrapper>
+        <Swiper {...params} ref={node => {
+            if (node) {
+              this.swiper = node.swiper;
+            }
+          }}>
+          {render(this.state.activeIndex)}
+        </Swiper>
+        <ThumbnailsWrapper>
+          <Thumbnails>
+            {renderThumbnails(this.updateCurrentSlide, activeIndex)}
+          </Thumbnails>
+        </ThumbnailsWrapper>
+      </PdpGalleryWrapper>;
   }
 }
 
