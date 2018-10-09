cc0a49b893d4f1c655b5fcbdef8b8ccf965dd3b9
Dan Hahn <daniel.hahn@michaelkors.com>
2018-10-09 12:33:31 -0400

    update forEach

--- src/components/pdp-gallery/desktop.jsx
+++ src/components/pdp-gallery/desktop.jsx
@@ -122,7 +122,9 @@ export default class PdpDesktop extends Component {
 
   checkIfImagesLoaded = () => {
     const _this = this;
-    this.photos.querySelectorAll("img").forEach(img =>
+    const images = this.photos.querySelectorAll("img");
+    const nodeListImages = [].slice.call(images);
+    nodeListImages.forEach(img =>
       img.addEventListener("load", function cb(event) {
         _this.setState({
           loadedImages: _this.state.loadedImages + 1
@@ -136,7 +138,7 @@ export default class PdpDesktop extends Component {
     this.setState(
       {
         totalItems: this.props.photos.length,
-        imageListPositions: Array.from(this.photos.querySelectorAll("img")).map(
+        imageListPositions: [...this.photos.querySelectorAll("img")].map(
           photo => photo.offsetTop
         )
       },
