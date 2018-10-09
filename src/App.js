55853d1393d11f77b4e548d7068de8656dbad250
Dan Hahn <daniel.hahn@michaelkors.com>
2018-10-09 10:53:21 -0400

    1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import React, { Component } from "react";
 import PdpGallery from "./components/pdp-gallery";
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  position: fixed;
-  @media screen and (min-width: 769px){
-    left: 750px;
-    top: 200px;
-  }
-  @media screen and (max-width: 768px){
-    left: 0;
-    right: 0;
-    bottom: 0;
-    padding: 1em;
-    background-color: lightblue;
-    z-index: 100;
-
-    select {
-      border: 1px solid black;
-      margin-left: 10px;
-      padding: 1em;
-    }
-  }
-`;
 
 class App extends Component {
   state = {
-    photos: [],
-    options: Array.of(...".".repeat(10))
+    photos: []
   };
   render() {
     const { photos } = this.state;
