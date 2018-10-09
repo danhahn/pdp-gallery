ae0a93a7c5abee2516e810ba0ad76c88e568bd8b
Dan Hahn <daniel.hahn@michaelkors.com>
2018-10-09 10:42:31 -0400

    put back app code

--- src/App.js
+++ src/App.js
@@ -28,8 +28,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <h1>One Big Test</h1> */}
-        raw
+        <PdpGallery photos={photos} />
       </div>
     );
   }
